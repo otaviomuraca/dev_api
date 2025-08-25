7d98c11a84d829200eef9e38786a0780fd6e53f5
Otavio Muraca <42158209+otaviomuraca@users.noreply.github.com>
2025-08-25 03:02:19 -0400

    Criação de requirements.txt e estudo e aplicação de RESTful na listagem de habilidades

---
+++ app_restful.py
@@ -0,0 +1,69 @@
+import json
+
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+
+from habilidades import Habilidades, ListaHabilidades
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+desenvolvedores = [
+    {'id': 1, 'nome':'Otavio',
+     'habilidades':['Python',"Flask"]},
+    {'id': 2, 'nome':'Rafa',
+     'habilidades':['Python',"Django"]},
+    {'id': 3, 'nome':'Mariana',
+     'habilidades':['JavaScript',"React","Node.js"]},
+    {'id': 4, 'nome':'Lucas',
+     'habilidades':['Java',"Spring Boot","SQL"]},
+    {'id': 5, 'nome':'Carla',
+     'habilidades':['C#',"ASP.NET","Azure"]},
+    {'id': 6, 'nome':'Diego',
+     'habilidades':['Python',"Pandas","NumPy","ETL"]},
+    {'id': 7, 'nome':'Fernanda',
+     'habilidades':['Go',"Docker","Kubernetes"]},
+    {'id': 8, 'nome':'João',
+     'habilidades':['PHP',"Laravel","MySQL"]},
+    {'id': 9, 'nome':'Beatriz',
+     'habilidades':['Ruby',"Rails","PostgreSQL"]},
+    {'id': 10, 'nome':'Gustavo',
+     'habilidades':['TypeScript',"Angular","Firebase"]}
+]
+
+
+class Desenvolvedor(Resource):
+    def get(self, id):
+        return desenvolvedores[id]
+
+    def put(self, id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return desenvolvedores[id]
+
+    def delete(self, id):
+        desenvolvedores.pop(id)
+        return f"desenvolvedor {id} deletado com sucesso."
+
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return desenvolvedores
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return desenvolvedores[posicao]
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>/')
+api.add_resource(ListaDesenvolvedores, '/dev/')
+api.add_resource(ListaHabilidades, '/habilidades/' )
+api.add_resource(Habilidades, '/habilidades/<int:id>' )
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ habilidades.py
@@ -0,0 +1,73 @@
+'''
+Acrescentar na API de habilidades (módulo habilidades) os métodos PUT, POST e DELETE
+
+O método POST deverá inserir uma nova habilidade na lista
+
+O método PUT a partir de um ID(identificação da posição) deverá alterar o nome da habilidade que está naquela posição
+
+O método DELETE deverá deletar uma habilidade que esteja na posição informada na requisição
+
+'''
+
+
+import json
+
+from flask import Flask, request
+from flask_restful import Resource
+
+lista_habilidades = [
+    {'id': 1, 'habilidade': "Python"},
+    {'id': 2, 'habilidade': "Flask"},
+    {'id': 3, 'habilidade': "Django"},
+    {'id': 4, 'habilidade': "JavaScript"},
+    {'id': 5, 'habilidade': "React"},
+    {'id': 6, 'habilidade': "Node.js"},
+    {'id': 7, 'habilidade': "Java"},
+    {'id': 8, 'habilidade': "Spring Boot"},
+    {'id': 9, 'habilidade': "SQL"},
+    {'id': 10, 'habilidade': "C#"},
+    {'id': 11, 'habilidade': "ASP.NET"},
+    {'id': 12, 'habilidade': "Azure"},
+    {'id': 13, 'habilidade': "Pandas"},
+    {'id': 14, 'habilidade': "NumPy"},
+    {'id': 15, 'habilidade': "ETL"},
+    {'id': 16, 'habilidade': "Go"},
+    {'id': 17, 'habilidade': "Docker"},
+    {'id': 18, 'habilidade': "Kubernetes"},
+    {'id': 19, 'habilidade': "PHP"},
+    {'id': 20, 'habilidade': "Laravel"},
+    {'id': 21, 'habilidade': "MySQL"},
+    {'id': 22, 'habilidade': "Ruby"},
+    {'id': 23, 'habilidade': "Rails"},
+    {'id': 24, 'habilidade': "PostgreSQL"},
+    {'id': 25, 'habilidade': "TypeScript"},
+    {'id': 26, 'habilidade': "Angular"},
+    {'id': 27, 'habilidade': "Firebase"}
+]
+
+class Habilidades(Resource):
+    def get(self, id):
+        return lista_habilidades[id]
+
+    def put(self, id):
+        dados = json.loads(request.data)
+        lista_habilidades[id] = dados
+        return lista_habilidades[id]
+
+    def delete(self, id):
+        posicao = id - 1
+        lista_habilidades.pop(posicao)
+        return f'Habilidade excluída com sucesso.'
+
+
+
+class ListaHabilidades(Resource):
+    def get(self):
+        return lista_habilidades
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(lista_habilidades)
+        dados['id'] =  posicao + 1
+        lista_habilidades.append(dados)
+        return lista_habilidades[posicao]
